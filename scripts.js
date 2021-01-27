0dc06b9d6ad182bcfe89af8f7572dccd9b25fe4a
ahmed <ahmed_u2@live.com>
2021-01-27 15:49:49 -0500

    added functionality to the reset button

--- scripts.js
+++ scripts.js
@@ -1,17 +1,52 @@
 // Create a 16x16 grid
 const sketchpad = document.querySelector('.sketchpad');
 
-let counter = 0;
-for (let i = 0; i < 16; i++){
-    for (let k = 0; k < 16; k++){
-        
-        let block = document.createElement('div');
-        block.classList.add('block');
-        sketchpad.appendChild(block);
-        counter++;
-        console.log(counter);
+function createGrid(size){
+    for (let i = 0; i < size; i++){
+        for (let k = 0; k < 16; k++){
+            
+            let block = document.createElement('div');
+            block.classList.add('block');
+            sketchpad.appendChild(block);
+        }
     }
-}   //finish creating grid
+    
+    //Add event listeners to all blocks and add the hover attribute.
+    let sketchpadDivs = document.querySelectorAll('.sketchpad div');
 
-let sketchpadDivs = document.querySelectorAll('.sketchpad div');
-sketchpadDivs.forEach(block => block.addEventListener('hover', ))
+    sketchpadDivs.forEach(block => 
+        block.addEventListener('mouseover', function(e){
+            block.style.backgroundColor = 'black';
+        }   
+    ));   
+} //end of createGrid()
+
+
+
+
+//Add event listener to the button
+let button = document.querySelector('button');
+
+button.addEventListener('click', function(e) {
+    while(true){
+        let size = parseInt(prompt("What grid size would you like?"));
+        console.log(size);
+        if (Number.isInteger(size) && size <= 100){
+            
+            //Remove the divs
+            let sketchpadDivs = document.querySelectorAll('.sketchpad div');
+            
+            sketchpadDivs.forEach(block => block.remove()); 
+
+            //Create a new grid
+            console.log("i made it");
+            createGrid(size);
+            
+            break;
+        } //end if
+
+    }//end while
+
+}); //end function(e)
+
+createGrid(16);
